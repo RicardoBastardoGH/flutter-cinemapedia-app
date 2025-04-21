7c71fc861499ea533e8a035dd4383c49a4d234af
Ricardo Bastardo <rbrodriguez.siaca@gmail.com>
2025-04-21 12:08:11 -0400

    riverpod y movie repo impl

---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Este es inmutable, no se puede modificar, es de solo lectura
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRepositoryImpl(datasource: MovieDbDatasource());
+});
+
